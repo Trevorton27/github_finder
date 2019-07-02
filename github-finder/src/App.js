342eb80c072effe5e8c716ea6e652bd72af2f647
Trevor Mearns <spiral272@gmail.com>
2019-07-02 17:11:50 +0900

    app is essentially complete. Now to refactor.

--- github-finder/src/App.js
+++ github-finder/src/App.js
@@ -1,8 +1,11 @@
-import React, {Component} from 'react'
+import React, {Component, Fragment} from 'react';
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Navbar from './Components/Layout/Navbar';
 import User from './Components/Users/User';
+import UserProfile from './Components/Users/UserProfile';
 import Search from './Components/Users/Search';
 import Alert from './Components/Layout/Alert';
+import About from './Components/pages/About';
 import axios from 'axios';
 import './App.css';
 
@@ -13,9 +16,12 @@ import './App.css';
 class App extends Component {
   state = {
     users: [],
+    user: {},
+    repos: [],
     loading: false,
     alert: null
-  }
+    
+  };
 
 
     //searches github users
@@ -27,8 +33,31 @@ class App extends Component {
     {process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
     
     this.setState({ users: response.data.items, loading: false });
-    console.log({ users: response.data.items} );
+    
   }
+ //get single github user
+ getUser = async (username) => {
+  this.setState({ loading: true });
+
+  const response = await axios.get(`https://api.github.com/users/${username}?client_id=$
+  {process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=$
+  {process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+  
+  this.setState({ user: response.data, loading: false });
+  console.log({ user: response.data} );
+ }
+
+ getUserRepos = async (username) => {
+  this.setState({ loading: true });
+
+  const response = await axios.get(`https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=$
+  {process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=$
+  {process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+  
+  this.setState({ repos: response.data, loading: false });
+  //console.log({ user: response.data} );
+ }
+
 
   clearUsers = () => this.setState({ users: [], loading: false });
 
@@ -41,25 +70,44 @@ class App extends Component {
 
   render() {
 
-  const {users, loading} = this.state;
+  const {users, user, loading, repos} = this.state;
  
   return (
+    <Router>
     <div className="App">
       <Navbar  />
       <div className="container">
         <Alert alert={this.state.alert} />
-        <Search 
-        searchUsers={this.searchUsers} 
-        clearUsers={this.clearUsers} 
-        showClear={users.length > 0 ? true : false }  
-        setAlert={this.setAlert}
-        />
-        <User 
-        loading={loading} 
-        users={users} 
-        />
+        <Switch>
+          <Route exact path='/' render={props => (
+            <Fragment>
+                 <Search 
+                  searchUsers={this.searchUsers} 
+                  clearUsers={this.clearUsers} 
+                  showClear={users.length > 0 ? true : false }  
+                  setAlert={this.setAlert}
+                  />
+                  <User 
+                  loading={loading} 
+                  users={users} 
+                  />
+            </Fragment>
+          )} />
+          <Route exact path='/about' component={About} />
+          <Route exact path ='/user/:login' render={props=> (
+            <UserProfile 
+            {...props } 
+            getUser={this.getUser}
+            getUserRepos={this.getUserRepos} 
+            user={ user} 
+            repos={repos}
+            loading={loading} />
+          )}/>
+        </Switch>
+     
       </div>
     </div>
+    </Router>
   );
  } 
 }
---
+++ github-finder/src/Components/pages/About.js
@@ -0,0 +1,15 @@
+import React, {Fragment} from 'react';
+
+const About = () => {
+    return (
+        <Fragment>
+            <h1>
+                About This App
+            </h1>
+            <p>Here, in this app of apps, thou art endowed with the ability to searcheth All Of Github for users by entering their username in Yon Field Of Search. Indeed, it is good.</p>
+            <p>Versioneth: I.0.0 </p>
+        </Fragment>
+    )
+}
+
+export default About
